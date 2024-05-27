e17ab7d06c3cc3795cd210db1d177d9be3bc919c
Gregory Morrison <greg.morrison@gmail.com>
2024-05-27 09:51:42 -0400

    Added color scripts.

---
+++ wsl/bash/258.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for i in {0..15}; do
+    printf '\e[48;5;%dm%3d ' $i $i;
+done
+printf '\e[0m\n';
+
+for h in {0..5}; do
+    for i in {0..5}; do
+        for j in {16..21}; do
+            let x=$((($h * 36) + ($i * 6) + $j))
+            printf '\e[48;5;%dm%3d ' $x $x
+        done
+    done
+    printf '\e[0m\n';
+done
+
+for i in {232..255}; do
+    printf '\e[48;5;%dm%3d ' $i $i;
+done
+printf '\e[0m\n';
+
